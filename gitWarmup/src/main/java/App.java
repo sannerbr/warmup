a9dbd79002c7b46726f2c30b1d005d7ba352fd1f
James Churchill <james@smashdev.com>
2020-10-13 12:23:47 -0700

    Update App class

--- gitWarmup/src/main/java/App.java
+++ gitWarmup/src/main/java/App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Nice job on testing, Brian");
+
+        System.out.println("Why should u buy a WiFi enabled espresso machine?");
+        System.out.println("To get the latest Java updates");
     }
 }
